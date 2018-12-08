3cfd4b7a43abf0760dabb66301696810b39914e6
Toni G <toni.giorgino@gmail.com>
2018-12-08 15:55:34 +0100

    wtf

--- scipy/signal/_dtw_utils.pyx
+++ scipy/signal/_dtw_utils.pyx
@@ -11,7 +11,7 @@ from cpython cimport array
 # Not sure this file is even necessary.
 
 
-__all__ = ["test_computeCM"]
+__all__ = ["dtw","_computeCM","_test_computeCM"]
 
 
 
@@ -27,36 +27,39 @@ cdef extern from "dtw_computeCM.h":
   ) 
 
 
-  
-def test_computeCM(TS=5):
 
-    DTYPE = np.int32
+  
+
+
+  
+def _computeCM(np.int[::1] s not None,
+               np.int[:,::1] wm not None,
+               np.float64_t[:,::1] lm not None,
+               np.int[:] nsteps not None,
+               np.float64_t[::1] dir not None,
+               np.float64_t[:,::1] cm not None,
+               np.int[:,::1] sm not None  ):
 
     # Size
-    cdef int [:] ts=np.array((TS, TS), dtype=DTYPE)
+    cdef int [:] ts=s
 
     # Window
-    cdef int [:,:] twm = np.ones((TS, TS), dtype=DTYPE)
+    cdef int [:,:] twm = wm
 
     # Local distance matrix
-    cdef double [:,:] tlm = np.zeros( (TS,TS), dtype=np.double)
-    for i in range(TS):
-        for j in range(TS):
-            tlm[i,j]=(i+1)*(j+1)
+    cdef double [:,:] tlm = lm
 
     # Step pattern size
-    cdef int [:] tnstepsp = np.array([6], dtype=DTYPE)
+    cdef int [:] tnstepsp = nsteps
 
     # Step pattern
-    cdef double [:] tdir = np.array( (1, 1, 2, 2, 3, 3, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0,-1, 1,-1, 1,-1, 1),
-                                     dtype=np.double)
+    cdef double [:] tdir = dir
 
     # Computed cumulative cost matrix
-    cdef double [:,:] tcm = np.full_like(tlm, np.nan, dtype=np.double)
-    tcm[0,0] = tlm[0,0]
+    cdef double [:,:] tcm = cm
 
     # Direction matrix
-    cdef int [:,:] tsm = np.full_like(tlm, -1, dtype=DTYPE)
+    cdef int [:,:] tsm = sm
 
     computeCM(&ts[0],
               &twm[0,0],
@@ -67,5 +70,39 @@ def test_computeCM(TS=5):
               &tsm[0,0])
 
     return (tcm, tsm)
+
+
+
+  
+  
+def _test_computeCM(TS=5):
+
+    ts=np.array((TS, TS), dtype=DTYPE)
+
+    twm = np.ones((TS, TS), dtype=DTYPE)
+
+    tlm = np.zeros( (TS,TS), dtype=np.double)
+    for i in range(TS):
+        for j in range(TS):
+            tlm[i,j]=(i+1)*(j+1)
+
+    tnstepsp = np.array([6], dtype=DTYPE)
+
+    tdir = np.array( (1, 1, 2, 2, 3, 3, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0,-1, 1,-1, 1,-1, 1),
+                                     dtype=np.double)
+
+    tcm = np.full_like(tlm, np.nan, dtype=np.double)
+    tcm[0,0] = tlm[0,0]
+
+    tsm = np.full_like(tlm, -1, dtype=DTYPE)
+
+    a, b = _computeCM(ts,
+                      twm,
+                      tlm,
+                      tnstepsp,
+                      tdir,
+                      tcm,
+                      tsm)
+    return (a,b)
     
     
