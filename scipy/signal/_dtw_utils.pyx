fb385719dbd02512f5514bfa6b06198d312ecd13
Toni G <toni.giorgino@gmail.com>
2018-12-08 15:00:41 +0100

    does something

--- scipy/signal/_dtw_utils.pyx
+++ scipy/signal/_dtw_utils.pyx
@@ -30,37 +30,33 @@ cdef extern from "dtw_computeCM.h":
   
 def test_computeCM(TS=5):
 
-    ts=np.array((TS, TS), dtype=np.int)
-    cdef array.array ts_c = array.array('i', ts)
+    cdef int [:] ts=np.array((TS, TS), dtype=np.int32)
 
-    twm = np.ones((TS, TS), dtype=np.int)
-    cdef array.array twm_c = array.array('i', twm)
+    cdef int [:,:] twm = np.ones((TS, TS), dtype=np.int32)
 
-    tlm = np.zeros( (TS,TS), dtype=np.double)
+    cdef double [:,:] tlm = np.zeros( (TS,TS), dtype=np.double)
     for i in range(TS):
         for j in range(TS):
             tlm[i,j]=(i+1)*(j+1)
-    cdef array.array tlm_c = array.array('d', tlm)
 
-    tnstepsp = [6]
-    cdef array.array tnstepsp_c = array.array('i', tnstepsp)
+    cdef int [:] tnstepsp = np.array([6], dtype=np.int32)
     
-    tdir = np.array( (1, 1, 2, 2, 3, 3, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0,-1, 1,-1, 1,-1, 1),
-                     dtype=np.double)
-    cdef array.array tdir_c = array.array('d', tdir)
+    cdef double [:] tdir = np.array( (1, 1, 2, 2, 3, 3, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0,-1, 1,-1, 1,-1, 1),
+                                     dtype=np.double)
 
-    tcm = np.full_like(tlm, np.nan, dtype=np.double)
+    cdef double [:,:] tcm = np.full_like(tlm, np.nan, dtype=np.double)
     tcm[0,0] = tlm[0,0]
-    cdef array.array tcm_c = array.array('d', tcm)
 
-    tsm = np.full_like(tlm, -1, dtype=np.int)
-    cdef array.array tsm_c = array.array('i', tsm)
+    cdef int [:,:] tsm = np.full_like(tlm, -1, dtype=np.int32)
 
-    computeCM(ts_c.data.as_ints,
-              twm_c.data.as_ints,
-              tlm_c.data.as_doubles,
-              tnstepsp_c.data.as_ints,
-              tdir_c.data.as_doubles,
-              tcm_c.data.as_doubles,
-              tsm_c.data.as_ints)
+    computeCM(&ts[0],
+              &twm[0,0],
+              &tlm[0,0],
+              &tnstepsp[0],
+              &tdir[0],
+              &tcm[0,0],
+              &tsm[0,0])
+
+    return (tcm, tsm)
+    
     
